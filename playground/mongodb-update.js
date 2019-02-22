5a04b82c5a70d70d469138bf7fb502cbe86f96f7
Mobin Ansari <mobin.ans@gmail.com>
2019-02-22 21:56:26 +0530

    update script

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -8,6 +8,12 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true
     }
     console.log('Connect to MongoDB server');
     const db = client.db('TodoApp');
+
+    db.collection('Todos').findOneAndUpdate({_id: new ObjectID('5c6f26ac8494e754813c45f7')}, 
+    {$set: { completed: true }}, {returnOriginal: false})
+    .then((result) => {
+        console.log(result);
+    });
     
     // client.close();
 });
